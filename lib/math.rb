fa0aa18df14b73472bede08e2698f74e6fc59430
bogomilmihaylov <bogomilmihaylov@live.com>
2019-08-12 11:39:26 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,26 @@
 def addition(num1, num2)
-
+sum = num1 + num2
+return sum
 end
 
 def subtraction(num1, num2)
-
+sum = num1 - num2
+return sum
 end
 
 def division(num1, num2)
-
+sum = num1 / num2
+return sum
 end
 
 def multiplication(num1, num2)
-
+sum = num1 * num2
+return sum
 end
 
 def modulo(num1, num2)
-
+sum = num1 / num2
+return sum
 end
 
 def square_root(num)
